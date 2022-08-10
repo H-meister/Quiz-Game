054cf794e6a0f92e6b85df239ae87b2230f6b7c8
H-meister <105766365+H-meister@users.noreply.github.com>
2022-08-09 22:44:55 -0400

    added HTML, CSS, and JS to file starting to get the funcionality to work.

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var btnStart = document.querySelector('#myBtn');
+
+var startQuiz = function() {
+    btnStart.addEventListener('click', function(){
+        //when button is clicked I want to start a timer using window.setTimeout(function,time)
+        
+    })
+}
+
+startQuiz();
