1e75f53f364f386f28df850bf31ed2698962e1b8
H-meister <Hector1626hg@gmail.com>
2022-08-10 12:02:07 -0400

    set up initial first page script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
-var btnStart = document.querySelector('#myBtn');
+var btnStart = document.getElementById('myBtn');
+var startPage = document.getElementById('startUp');
+var pageOne = document.querySelector('.firstPage')
+
+var displayPageOne = function(){
+    pageOne.style.display ="";
+}
 
 var startQuiz = function() {
     btnStart.addEventListener('click', function(){
         //when button is clicked I want to start a timer using window.setTimeout(function,time)
-        
+        console.log('this button is working');
+        startPage.remove();
+        displayPageOne();
     })
 }
 
